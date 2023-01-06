52e59fc3aaea1d2bc41b241c80bc40f4043b8c4e
Krishana Pal <krishnasingh.ks62@gmail.com>
2023-01-06 22:20:37 +0530

    First commit

---
+++ NamesteDuniya.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    // this code for print the line  endl for new line or '\n' for new line
+    cout << "Namaste Dunia :-)" <<endl;
+    // also work --> ; sami collen for end of the line
+    //cout << "Namaste Dunia :-)" <<'\n';
+    // foe new line 
+    // cout << '\n'; also work
+
+    // Data Type and Variables in C++
+    // int a = 5;  int is data type , a is variable and 5 is value 
+    // genrealy int 4 byte consume  4 byte = 32 bit
+
+    // char ch = 'a'; char  is data type, ch is a variable, 'a' is value of an data
+    // c++ in char data is a 1 byte consume and 1 byte = 8 bit
+
+    // boolean has a two valus true((1) and false(0) 
+    // boolean 1 bit  bool b = 1;
+
+    // float f = 1.2; float 8 byte consume , 8 byte = 64 bit
+    // double d = 1.23; double same as a float same byte consume , 8 byte = 64
+    
+
+
+}
